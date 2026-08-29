117c633da41d2d3e41584f8f02f8105a271e1aa0
Nishant01k <merobook01k@gmail.com>
2026-08-29 13:52:53 +0545

    2026.1.2

--- src/main/kotlin/org/viw/adintegrationpro/actions/EnterLicenseAction.kt
+++
@@ -1,6 +0,0 @@
-package org.viw.adintegrationpro.actions
-
-import com.intellij.openapi.actionSystem.AnAction
-import com.intellij.openapi.actionSystem.AnActionEvent
-import org.viw.adintegrationpro.ui.UpgradeDialog
-
